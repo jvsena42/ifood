0bd43a5d973f2215184a5fcc43bef63dd6bf81b7
jvsena42 <jvsena16@gmail.com>
2020-05-19 21:56:21 -0300

    configurando o projeto

---
+++ app/src/main/java/com/balatech/ifood/activity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.balatech.ifood.activity;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.balatech.ifood.R;
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
