f73ffddb1cc84feb3ac138570249cdb90f4b0190
jvsena42 <jvsena16@gmail.com>
2020-05-23 15:52:30 -0300

    Listando e excluindo produtos

--- app/src/main/java/com/balatech/ifood/activity/EmpresaActivity.java
+++ app/src/main/java/com/balatech/ifood/activity/EmpresaActivity.java
@@ -1,33 +1,119 @@
 package com.balatech.ifood.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.balatech.ifood.R;
+import com.balatech.ifood.adapter.AdapterProduto;
 import com.balatech.ifood.helper.ConfiguracaoFirebase;
+import com.balatech.ifood.helper.RecyclerItemClickListener;
+import com.balatech.ifood.helper.UsuarioFirebase;
+import com.balatech.ifood.model.Produto;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EmpresaActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
+    private RecyclerView recyclerProdutos;
+    private AdapterProduto adapterProduto;
+    private List<Produto> produtos = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private String idUsuarioLogado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
 
+        //Configurações iniciais
+        inicializarComponentes();
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Configurar toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Ifood - Empresa");
         setSupportActionBar(toolbar);
+
+        //Configurar recyclerview
+        recyclerProdutos.setLayoutManager(new LinearLayoutManager(this));
+        recyclerProdutos.setHasFixedSize(true);
+        adapterProduto = new AdapterProduto(produtos,this);
+        recyclerProdutos.setAdapter(adapterProduto);
+
+        //Recuperar produtos
+        recuperarProdutos();
+
+        //Adicionar evento de clique no recyclerview
+        recyclerProdutos.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        this,
+                        recyclerProdutos,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+                                Produto produtoSelecionado = produtos.get(position);
+                                produtoSelecionado.remover();
+                                Toast.makeText(EmpresaActivity.this, "Produto removido com sucesso!", Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        }
+                )
+        );
+    }
+
+
+    private void recuperarProdutos(){
+        DatabaseReference produtosRef = firebaseRef.child("produtos").child(idUsuarioLogado);
+
+        produtosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                produtos.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    produtos.add(ds.getValue(Produto.class));
+                }
+                adapterProduto.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void inicializarComponentes(){
+        recyclerProdutos = findViewById(R.id.recyclerProdutos);
     }
 
     @Override
