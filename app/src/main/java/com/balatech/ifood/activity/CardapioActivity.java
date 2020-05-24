784ac3f89566ab394e19eb06c573c1a23c4c5ce5
jvsena42 <jvsena16@gmail.com>
2020-05-24 11:05:08 -0300

    Adicionando itens do pedido

--- app/src/main/java/com/balatech/ifood/activity/CardapioActivity.java
+++ app/src/main/java/com/balatech/ifood/activity/CardapioActivity.java
@@ -6,7 +6,11 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,8 +18,10 @@ import com.balatech.ifood.R;
 import com.balatech.ifood.adapter.AdapterEmpresa;
 import com.balatech.ifood.adapter.AdapterProduto;
 import com.balatech.ifood.helper.ConfiguracaoFirebase;
+import com.balatech.ifood.helper.UsuarioFirebase;
 import com.balatech.ifood.model.Empresa;
 import com.balatech.ifood.model.Produto;
+import com.balatech.ifood.model.Usuario;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,6 +31,8 @@ import com.squareup.picasso.Picasso;
 import java.util.ArrayList;
 import java.util.List;
 
+import dmax.dialog.SpotsDialog;
+
 public class CardapioActivity extends AppCompatActivity {
 
     private RecyclerView recyclerProdutosCardapio;
@@ -32,10 +40,13 @@ public class CardapioActivity extends AppCompatActivity {
     private TextView textNomeEmpresaCardapio;
     private Empresa empresaSelecionada;
     private String idEmpresa;
+    private AlertDialog dialog;
+    private Usuario usuario;
 
     private AdapterProduto adapterProduto;
     private List<Produto> produtos = new ArrayList<>();
     private DatabaseReference firebaseRef;
+    private String idUsuarioLogado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +56,7 @@ public class CardapioActivity extends AppCompatActivity {
         //Configurações iniciais
         inicializarComponentes();
         firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idUsuarioLogado = UsuarioFirebase.getIdUsuario();
 
         //Recuperar empresa selecionada
         Bundle bundle = getIntent().getExtras();
@@ -68,11 +80,68 @@ public class CardapioActivity extends AppCompatActivity {
         //Recuperar produtos
         recuperarProdutos();
 
+        //Racuperar dados do usuário
+        recuperarDadosUsuario();
+
         //Configurar recyclervire
         recyclerProdutosCardapio.setLayoutManager(new LinearLayoutManager(this));
         recyclerProdutosCardapio.setHasFixedSize(true);
         adapterProduto = new AdapterProduto(produtos,this);
         recyclerProdutosCardapio.setAdapter(adapterProduto);
+    }
+
+    private void recuperarDadosUsuario() {
+
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando dados")
+                .setCancelable(false)
+                .build();
+        dialog.show();
+
+        DatabaseReference usuariosRef = firebaseRef
+                .child("usuarios")
+                .child(idUsuarioLogado);
+
+        usuariosRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getValue() != null){
+                    usuario = dataSnapshot.getValue(Usuario.class);
+                }
+                recuperarPedido();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void recuperarPedido(){
+        dialog.dismiss();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_cardapio,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuPedido:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void recuperarProdutos(){
